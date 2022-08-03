634a66966456877416c87cabca3770e311bce955
Đỗ Ngọc Ngoạn <ngocngoan060288@gmail.com>
2022-08-03 22:50:52 +0700

    update token backend to be compatible with django rest framework 4.8.0

--- rest_framework_simplejwt_mongoengine/state.py
+++ rest_framework_simplejwt_mongoengine/state.py
@@ -1,6 +1,23 @@
 from rest_framework_simplejwt.backends import TokenBackend
 
 from .settings import api_settings
+from .utils import drf_simplejwt_version
 
-token_backend = TokenBackend(api_settings.ALGORITHM, api_settings.SIGNING_KEY,
-                             api_settings.VERIFYING_KEY, api_settings.AUDIENCE, api_settings.ISSUER)
+if drf_simplejwt_version in ["4.7.0", "4.7.1", "4.7.2"]:
+    token_backend = TokenBackend(
+        api_settings.ALGORITHM,
+        api_settings.SIGNING_KEY,
+        api_settings.VERIFYING_KEY,
+        api_settings.AUDIENCE,
+        api_settings.ISSUER,
+    )
+else:
+    token_backend = TokenBackend(
+        api_settings.ALGORITHM,
+        api_settings.SIGNING_KEY,
+        api_settings.VERIFYING_KEY,
+        api_settings.AUDIENCE,
+        api_settings.ISSUER,
+        api_settings.JWK_URL,
+        api_settings.LEEWAY,
+    )
