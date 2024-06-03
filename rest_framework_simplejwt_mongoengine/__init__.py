8ec992574bb9b928b1a458d6d1cda970b607b6d9
Đỗ Ngọc Ngoạn <ngocngoan060288@gmail.com>
2024-06-03 07:03:48 +0000

    fix project version

--- rest_framework_simplejwt_mongoengine/__init__.py
+++ rest_framework_simplejwt_mongoengine/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("djangorestframework_simplejwt_mongoengine")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = None
+
+
 __author__ = """Đỗ Ngọc Ngoạn (aka Ngoan Do)"""
 __email__ = "ngocngoan060288@gmail.com"
-__version__ = "1.3.0"
